f24f74f3a92d465f467ca0d13948c0bc72d9fa6f
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2023-08-15 18:08:44 +0100

    edit

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -30,6 +30,21 @@ class PetsController < ApplicationController
     end
   end
 
+  def edit
+    # get the pet from the id in the URL
+    @pet = Pet.find(params[:id])
+  end
+
+  def update
+    # find the pet instance from the params
+    @pet = Pet.find(params[:id])
+    # update the instance with the new params (strong)
+    @pet.update(pet_params)
+
+    # send the user to show page
+    redirect_to pet_path(@pet)
+  end
+
   private
 
   def pet_params
