f70d6e2892a44b67027423229b12dc5ed4cb1cf8
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2023-08-15 17:42:39 +0100

    show done

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -3,4 +3,10 @@ class PetsController < ApplicationController
     # get all the pets from the DB
     @pets = Pet.all
   end
+
+  def show
+    # find a pet instance with the id
+    # Assign that to an instance variable so i can render them in view
+    @pet = Pet.find(params[:id])
+  end
 end
